dcaf2c984dd5866520488fb212e931b5d8f20fc5
arunmehnadi <arunmehndichennai@gmail.com>
2025-02-06 01:31:53 +0530

    Update Logo.tsx

--- component/Header/Logo.tsx
+++ component/Header/Logo.tsx
@@ -3,11 +3,11 @@ import { Flower2 } from 'lucide-react';
 import Link from 'next/link';
 export default function Logo() {
   return (
-    <div className="py-5 px-6 ">
-      <Link href="/" className="inline-flex items-center gap-2 group">
+    <div className="pt-6">
+      <Link href="/" className="flex justify-center items-center space-x-2 group">
         <img src='/assets/logo.png' className="w-20 h-20 text-henna-600 group-hover:text-henna-700 transition-colors" />
         <span className="font-serif text-2xl md:text-3xl font-bold text-henna-800 group-hover:text-henna-900 transition-colors">
-          Arun Mehandi Artist
+          Chennai Mehndi Artist
         </span>
       </Link>
     </div>
